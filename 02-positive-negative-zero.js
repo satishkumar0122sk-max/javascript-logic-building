b00f5e690d208f2994973d0060efc73aa42459eb
satishkumar0122sk-max <satishkumar0122sk@gmail.com>
2026-08-25 22:39:16 +0530

    Create 02-positive-negative-zero.js

---
+++ 02-positive-negative-zero.js
@@ -0,0 +1,10 @@
+// ===== Question 2: Positive, Negative or Zero =====
+
+let num = +prompt("write your number");
+if (num === 0){
+console.log("zero");
+} else if (num > 0) {
+console.log("positive");
+} else {
+console.log("negative");
+}
