6787e64710db62de3fae969af203fb594c48668c
Satish Pal <satishkumar0122sk@gmail.com>
2026-08-26 11:00:35 +0530

    Add files via upload

---
+++ 05-divisible-by-5.js
@@ -0,0 +1,10 @@
+// Question:
+// Check whether a number is divisible by 5.
+
+let num = +prompt("Write your number");
+
+if (num % 5 === 0) {
+  console.log("yes");
+} else {
+  console.log("no");
+}
